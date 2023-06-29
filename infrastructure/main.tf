45201137b7dba0fdc567de726dc6e5f070563af3
tomondre <tomas.ondrejka2000@gmail.com>
2023-06-29 14:42:50 +0200

    removing cron job

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,16 +8,16 @@ terraform {
   }
 }
 
-module "star_deployment" {
-  source    = "git::https://github.com/tomondre/raspberry-kubernetes-cluster.git//terraform-modules/reusable-modules/cron-job"
-  image_tag = var.image_tag
-  image_url = "docker.io/tomondre/you-are-my-star"
-  name      = "you-are-my-star"
-  cron      = "*/10 * * * *"
-  env       = {
-    GITHUB_TOKEN = var.github_token
-  }
-}
+#module "star_deployment" {
+#  source    = "git::https://github.com/tomondre/raspberry-kubernetes-cluster.git//terraform-modules/reusable-modules/cron-job"
+#  image_tag = var.image_tag
+#  image_url = "docker.io/tomondre/you-are-my-star"
+#  name      = "you-are-my-star"
+#  cron      = "*/10 * * * *"
+#  env       = {
+#    GITHUB_TOKEN = var.github_token
+#  }
+#}
 
 variable "image_tag" {}
 variable "github_token" {}
